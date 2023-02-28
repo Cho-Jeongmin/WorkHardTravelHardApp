aeca166f75bf182963307ada44268c76c425da30
Jeongmin Cho <jojm0829@gmail.com>
2023-03-01 00:15:02 +0900

    #3.1 Touchables

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TouchableHighlight,
+  TouchableWithoutFeedback,
+} from "react-native";
+import { theme } from "./colors";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity activeOpacity={0}>
+          <Text style={styles.btnText}>Work</Text>
+        </TouchableOpacity>
+        <TouchableWithoutFeedback
+          onPress={() => {
+            console.log("pressed");
+          }}
+        >
+          <Text style={styles.btnText}>Travel</Text>
+        </TouchableWithoutFeedback>
+      </View>
     </View>
   );
 }
@@ -13,8 +32,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.bg,
+    paddingHorizontal: 20,
+  },
+  header: {
+    marginTop: 100,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    width: "100%",
+  },
+  btnText: {
+    fontSize: 35,
+    color: "white",
+    fontWeight: 600,
   },
 });
