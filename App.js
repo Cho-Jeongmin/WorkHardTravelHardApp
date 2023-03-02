a3fb42d5832c199acfa7d1e9435b3ded045304ce
Jeongmin Cho <jojm0829@gmail.com>
2023-03-02 14:47:49 +0900

    #3.3 To Dos

--- App.js
+++ App.js
@@ -12,9 +12,19 @@ import { theme } from "./colors";
 export default function App() {
   const [working, setWorking] = useState(true);
   const [text, setText] = useState("");
+  const [toDos, setToDos] = useState({});
   const travel = () => setWorking(false);
   const work = () => setWorking(true);
   const onChangeText = (payload) => setText(payload);
+  const addToDo = (event) => {
+    if (text === "") return;
+    const newToDos = Object.assign({}, toDos, {
+      [Date.now()]: { text, work: working },
+    });
+    setToDos(newToDos);
+    setText("");
+  };
+  console.log(toDos);
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
@@ -41,6 +51,8 @@ export default function App() {
         style={styles.input}
         value={text}
         onChangeText={onChangeText}
+        onSubmitEditing={addToDo}
+        returnKeyType="done"
         placeholder={working ? "Add a To Do" : "Where do you want to go?"}
       />
     </View>
